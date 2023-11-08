fe7f0fe5b3a81bb51a04eba1635cac1e88e4f2bc
jdvictoria <133762589+jdvictoria@users.noreply.github.com>
2023-11-09 00:12:07 +0800

    Migrate Component

---
+++ components/molecules/formbox/index.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {StyledCol} from '../../../styles/container';
+import {
+  contentText,
+  inputText,
+  StyledText16,
+  StyledText20,
+  StyledText34,
+} from '../../../styles/text';
+
+const FormBox = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 85%;
+  height: 45%;
+
+  border: 2px;
+  border-radius: 30px;
+`;
+
+const FormButton = styled.TouchableOpacity`
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+
+  bottom: 0;
+  height: 15%;
+  width: 100%;
+
+  border-bottom-left-radius: 35px;
+  border-bottom-right-radius: 35px;
+
+  background: #518bfe;
+`;
+
+const FormInput = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 87.5%;
+`;
+
+const FormTextInput = styled.TextInput`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 60px;
+  width: 100%;
+
+  margin-top: 10px;
+  margin-bottom: 25px;
+  padding-left: 25px;
+
+  border-radius: 10px;
+
+  font-size: 16px;
+  color: #94a4ac;
+  background-color: #1a2230;
+`;
+
+// @ts-ignore
+function FormComponent({isDarkMode}) {
+  const contentStyle = contentText(isDarkMode);
+  const inputStyle = inputText(isDarkMode);
+
+  return (
+    <FormBox
+      style={{
+        backgroundColor: isDarkMode ? '#010919' : '#ffffff',
+        borderColor: isDarkMode ? '#010919' : '#ffffff',
+        marginTop: 40,
+        marginBottom: 30,
+      }}>
+      <StyledCol style={{marginBottom: 25}}>
+        <StyledText34 style={inputStyle.semibold}>Sign Up</StyledText34>
+      </StyledCol>
+      <FormInput style={{marginBottom: 35}}>
+        <StyledText20 style={inputStyle.semibold}>Email</StyledText20>
+        <FormTextInput
+          style={{backgroundColor: isDarkMode ? '#1a2230' : '#f8faff'}}
+          placeholder="useless placeholder"
+          placeholderTextColor="#9fa4ac"
+        />
+        <StyledText20 style={inputStyle.semibold}>Password</StyledText20>
+        <FormTextInput
+          style={{backgroundColor: isDarkMode ? '#1a2230' : '#f8faff'}}
+          placeholder="useless placeholder"
+          placeholderTextColor="#9fa4ac"
+        />
+      </FormInput>
+      <FormButton>
+        <StyledText16 style={[contentStyle.semibold, {color: 'white'}]}>
+          Sign In
+        </StyledText16>
+      </FormButton>
+    </FormBox>
+  );
+}
+
+export default FormComponent;
--- components/organism/sign-in/index.tsx
+++ components/organism/sign-in/index.tsx
@@ -1,78 +1,14 @@
 import React from 'react';
 import {Image, TouchableOpacity} from 'react-native';
 
-import styled from 'styled-components';
 import {StyledCol, StyledRow, StyledSafeView} from '../../../styles/container';
-import {
-  contentText,
-  inputText,
-  StyledText16,
-  StyledText20,
-  StyledText34,
-  StyledText40,
-} from '../../../styles/text';
+import {contentText, StyledText16, StyledText40} from '../../../styles/text';
 
-const FormBox = styled.View`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  width: 85%;
-  height: 45%;
-
-  border: 2px;
-  border-radius: 30px;
-`;
-
-const FormButton = styled.TouchableOpacity`
-  display: flex;
-  position: absolute;
-  justify-content: center;
-  align-items: center;
-
-  bottom: 0;
-  height: 15%;
-  width: 100%;
-
-  border-bottom-left-radius: 35px;
-  border-bottom-right-radius: 35px;
-
-  background: #518bfe;
-`;
-
-const FormInput = styled.View`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  width: 87.5%;
-`;
-
-const FormTextInput = styled.TextInput`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 60px;
-  width: 100%;
-
-  margin-top: 10px;
-  margin-bottom: 25px;
-  padding-left: 25px;
-
-  border-radius: 10px;
-
-  font-size: 16px;
-  color: #94a4ac;
-  background-color: #1a2230;
-`;
+import FormComponent from '../../molecules/formbox';
 
 // @ts-ignore
 function SignIn({isDarkMode}) {
   const contentStyle = contentText(isDarkMode);
-  const inputStyle = inputText(isDarkMode);
 
   return (
     <StyledSafeView>
@@ -86,36 +22,7 @@ function SignIn({isDarkMode}) {
           Psy-DD
         </StyledText40>
       </StyledCol>
-      <FormBox
-        style={{
-          backgroundColor: isDarkMode ? '#010919' : '#ffffff',
-          borderColor: isDarkMode ? '#010919' : '#ffffff',
-          marginTop: 40,
-          marginBottom: 30,
-        }}>
-        <StyledCol style={{marginBottom: 25}}>
-          <StyledText34 style={inputStyle.semibold}>Sign Up</StyledText34>
-        </StyledCol>
-        <FormInput style={{marginBottom: 35}}>
-          <StyledText20 style={inputStyle.semibold}>Email</StyledText20>
-          <FormTextInput
-            style={{backgroundColor: isDarkMode ? '#1a2230' : '#f8faff'}}
-            placeholder="useless placeholder"
-            placeholderTextColor="#9fa4ac"
-          />
-          <StyledText20 style={inputStyle.semibold}>Password</StyledText20>
-          <FormTextInput
-            style={{backgroundColor: isDarkMode ? '#1a2230' : '#f8faff'}}
-            placeholder="useless placeholder"
-            placeholderTextColor="#9fa4ac"
-          />
-        </FormInput>
-        <FormButton>
-          <StyledText16 style={[contentStyle.semibold, {color: 'white'}]}>
-            Sign In
-          </StyledText16>
-        </FormButton>
-      </FormBox>
+      <FormComponent isDarkMode={isDarkMode} />
       <StyledRow style={{marginBottom: 25}}>
         <TouchableOpacity>
           <StyledText16 style={[contentStyle.bold, {color: 'white'}]}>
