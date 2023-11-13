ba6bf8de3500285f1868764efb0450a0df7c779e
jdvictoria <150222311+builtbysandbox@users.noreply.github.com>
2023-11-14 00:03:47 +0800

    Finalize Nav Bar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import React, {useState} from 'react';
 
-import {StyledSafeView} from './styles/input-container';
-
 import {
   AnimatedTabBarNavigator,
-  DotSize, // optional
-  TabElementDisplayOptions, // optional
-  TabButtonLayout, // optional
-  IAppearanceOptions, // optional
+  DotSize,
+  TabElementDisplayOptions,
 } from 'react-native-animated-nav-tab-bar';
-
-import UserAuth from './components/organism/1_user-auth';
 import HomeProfile from './components/organism/2_home/b_profile';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeDiagnose from './components/organism/2_home/a_diagnose';
 import HomeSettings from './components/organism/2_home/c_settings';
+import {Image} from 'react-native';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -24,14 +19,79 @@ function App() {
   return (
     <NavigationContainer>
       <Tabs.Navigator
-        // default configuration from React Navigation
-        tabBarOptions={{
-          activeTintColor: '#2F7C6E',
-          inactiveTintColor: '#222222',
+        appearance={{
+          floating: true,
+          shadow: true,
+          whenActiveShow: TabElementDisplayOptions.ICON_ONLY,
+          whenInactiveShow: TabElementDisplayOptions.ICON_ONLY,
+          dotSize: DotSize.SMALL,
+          dotCornerRadius: 30,
+          tabBarBackground: isDarkMode ? '#1a2230' : '#ffffff',
+          activeTabBackgrounds: '#518cff',
         }}>
-        <Tabs.Screen name="HomeDiagnose" component={HomeDiagnose} />
-        <Tabs.Screen name="HomeProfile" component={HomeProfile} />
-        <Tabs.Screen name="HomeSettings" component={HomeSettings} />
+        <Tabs.Screen
+          name="Diagnose"
+          options={{
+            tabBarIcon: () => (
+              <Image
+                style={{
+                  width: 25,
+                  height: 25,
+                  resizeMode: 'contain',
+                }}
+                source={
+                  isDarkMode
+                    ? require('./assets/icons/diagnose-icon_dark.png')
+                    : require('./assets/icons/diagnose-icon.png')
+                }
+                alt={'Diagnose'}
+              />
+            ),
+          }}>
+          {props => <HomeDiagnose {...props} isDarkMode={isDarkMode} />}
+        </Tabs.Screen>
+        <Tabs.Screen
+          name="Profile"
+          options={{
+            tabBarIcon: () => (
+              <Image
+                style={{
+                  width: 25,
+                  height: 25,
+                  resizeMode: 'contain',
+                }}
+                source={
+                  isDarkMode
+                    ? require('./assets/icons/profile_dark.png')
+                    : require('./assets/icons/profile.png')
+                }
+                alt={'Diagnose'}
+              />
+            ),
+          }}>
+          {props => <HomeProfile {...props} isDarkMode={isDarkMode} />}
+        </Tabs.Screen>
+        <Tabs.Screen
+          name="Settings"
+          options={{
+            tabBarIcon: () => (
+              <Image
+                style={{
+                  width: 25,
+                  height: 25,
+                  resizeMode: 'contain',
+                }}
+                source={
+                  isDarkMode
+                    ? require('./assets/icons/settings_dark.png')
+                    : require('./assets/icons/settings.png')
+                }
+                alt={'Diagnose'}
+              />
+            ),
+          }}>
+          {props => <HomeSettings {...props} isDarkMode={isDarkMode} />}
+        </Tabs.Screen>
       </Tabs.Navigator>
     </NavigationContainer>
   );
