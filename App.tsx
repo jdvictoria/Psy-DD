25b987a6b649dc01cab1af480e07d1f957ddf456
Joshua Victoria <jdvictoria.tech@gmail.com>
2023-11-25 23:03:56 +0800

    Name Fetch Validity

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@ import {StyledSafeView} from './styles/input-container';
 import UserAuth from './components/organism/1_user-auth';
 import HomeNavigation from './components/organism/2_home';
 import Loading from './components/organism/3_loading';
-import {WebView} from 'react-native-webview';
+
+import {WebView, WebViewMessageEvent} from 'react-native-webview';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -34,6 +35,8 @@ function App() {
   const webViewRef = useRef(null);
   const [openWeb, setOpenWeb] = useState(false);
 
+  const [isValid, setIsValid] = useState(false);
+
   const handleWebViewLoad = () => {
     if (webViewRef.current) {
       const injectScript = byLicense
@@ -61,38 +64,62 @@ function App() {
         }, 500);
       `
         : `
-        document.querySelector('a[href="#profile"]').click();
-        
-        setTimeout(() => {
-          const selectElement = document.querySelector('.form-control.ddselect.profs.select2-hidden-accessible#verNaProf');
-          if (selectElement) {
-            // Simulate selecting the option with value "57"
-            const optionToSelect = selectElement.querySelector('option[value="57"][data-select2-id="140"]');
-            if (optionToSelect) {
-              optionToSelect.selected = true;
-              selectElement.dispatchEvent(new Event('change', { bubbles: true }));
-            }
-          }
-          
-          var fnameInput = document.getElementById('verNaFname');
-          fnameInput.value = '${firstName}';
-          
-          var lnameInput = document.getElementById('verNaLname');
-          lnameInput.value = '${lastName}';
-          
-          setTimeout(() => {
-            var verifyButton = document.getElementById('verifyNAbtn');
-            if (verifyButton) {
-              verifyButton.click();
-            }
-          }, 500);
-        }, 500);
-      `;
+    document.querySelector('a[href="#profile"]').click();
+    
+    setTimeout(() => {
+      const selectElement = document.querySelector('.form-control.ddselect.profs.select2-hidden-accessible#verNaProf');
+      if (selectElement) {
+        // Simulate selecting the option with value "57"
+        const optionToSelect = selectElement.querySelector('option[value="57"][data-select2-id="140"]');
+        if (optionToSelect) {
+          optionToSelect.selected = true;
+          selectElement.dispatchEvent(new Event('change', { bubbles: true }));
+        }
+      }
+      
+      var fnameInput = document.getElementById('verNaFname');
+      fnameInput.value = '${firstName}';
+      
+      var lnameInput = document.getElementById('verNaLname');
+      lnameInput.value = '${lastName}';
+      
+      setTimeout(() => {
+        var verifyButton = document.getElementById('verifyNAbtn');
+        if (verifyButton) {
+          verifyButton.click();
+        }
+      }, 500);
+    }, 500);
+
+    // Watch for changes in the display of the modal
+    const targetDiv = document.getElementById('myModalVerify');
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === 'style' && targetDiv.style.display === 'block') {
+          // Send a message to indicate that the display has changed to "block"
+          window.ReactNativeWebView.postMessage('myModalVerifyDisplayChanged');
+        }
+      });
+    });
+
+    observer.observe(targetDiv, { attributes: true });
+  `;
 
       webViewRef.current.injectJavaScript(injectScript);
     }
   };
 
+  const handleWebViewMessage = (event: WebViewMessageEvent) => {
+    if (event.nativeEvent.data === 'myModalVerifyDisplayChanged') {
+      setIsValid(true);
+    }
+  };
+
+  useEffect(() => {
+    if (isValid) {
+      console.log(isValid);
+    }
+  }, [isValid]);
   return !isLoggedIn ? (
     <>
       <UserAuth
@@ -119,6 +146,7 @@ function App() {
           source={{uri: 'https://online.prc.gov.ph/Verification'}}
           style={{bottom: 0}}
           onLoad={handleWebViewLoad}
+          onMessage={handleWebViewMessage}
         />
       )}
     </>
