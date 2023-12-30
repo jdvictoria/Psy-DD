afc491e00f5c9540b047fab07763c0bb9835f72f
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2023-12-30 22:30:06 +0800

    Create neurodevelopmental.js

---
+++ utils/neurodevelopmental.js
@@ -0,0 +1,203 @@
+export const neurodevelopmentalSymptoms = [
+    // Intellectual Developmental Disorder
+    // Criteria A
+    {label: 'Deficits in intellectual functions, such as reasoning, problem solving, planning, abstract thinking, judgment, academic learning, and learning from experience', value: 1},
+
+    // Criteria B
+    {label: 'Deficits in adaptive functioning that result in failure to meet developmental and sociocultural standards for personal independence and social responsibility', value: 2},
+
+    // Criteria C
+    {label: 'Onset of intellectual and adaptive deficits during the developmental period', value: 3},
+
+    // Global Developmental Delay
+    // This diagnosis is reserved for individuals under the age of 5 years when the clinical severity level cannot be reliably assessed during early childhood. This category is diagnosed when an individual fails to meet expected developmental milestones in several areas of intellectual functioning, and applies to individuals who are unable to undergo systematic assessments of intellectual functioning, including children who are too young to participate in standardized testing. This category requires reassessment after a period of time.
+
+    // Unspecified Intellectual Developmental Disorder
+    // "This category is reserved for individuals over the age of 5 years when assessment of the degree of intellectual developmental disorder (intellectual disability) by means of locally available procedures is rendered difficult or impossible because of associated sensory or physical impairments, as in blindness or prelingual deafness; locomotor disability; or presence of severe problem behaviors or co-occurring mental disorder. This category should only be used in exceptional circumstances and requires reassessment after a period of time.
+
+    // Language Disorder
+    // Criteria A
+    {label: 'Reduced vocabulary', value: 4},
+    {label: 'Limited sentence structure', value: 5},
+    {label: 'Impairment in discourse', value: 6},
+
+    // Criteria B
+    {label: 'Language abilities are substantially and quantifiably below those expected for age', value: 7},
+
+    // Criteria C
+    {label: 'Onset of symptoms is in the early developmental period', value: 8},
+
+    // Criteria D
+    {label: 'The difficulties are not attributable to hearing or other sensory impairment, motor dysfunction, or another medical or neurological condition and are not better explained by intellectual developmental disorder', value: 9},
+
+    // Speech Sound Disorder
+    // Criteria A
+    {label: 'Persistent difficulty with speech sound production that interferes with speech intelligibility or prevents verbal communication of messages', value: 10},
+
+    // Criteria B
+    {label: 'The disturbance causes limitations in effective communication that interfere with social participation, academic achievement, or occupational performance, individually or in any combination', value: 11},
+
+    // Criteria C
+    {label: 'Onset of symptoms is in the early developmental period', value: 12},
+
+    // Criteria D
+    {label: 'The difficulties are not attributable to congenital or acquired conditions', value: 13},
+
+    // Childhood-Onset Fluency Disorder (Stuttering)
+    // Criteria A
+    {label: 'Sound and syllable repetitions', value: 14},
+    {label: 'Sound prolongations of consonants as well as vowels', value: 15},
+    {label: 'Broken words', value: 16},
+    {label: 'Audible or silent blocking', value: 17},
+    {label: 'Circumlocutions', value: 18},
+    {label: 'Words produced with an excess of physical tension', value: 19},
+    {label: 'Monosyllabic whole-word repetitions', value: 20},
+
+    // Criteria B
+    {label: 'The disturbance causes anxiety about speaking or limitations in effective communication, social participation, or academic or occupational performance, individually or in any combination', value: 21},
+
+    // Criteria C
+    {label: 'The onset of symptoms is in the early developmental period. (Note: Later-onset cases are diagnosed as F98.5 adult-onset fluency disorder.)', value: 22},
+
+    // Criteria D
+    {label: 'The disturbance is not attributable to a speech-motor or sensory deficit, dysfluency associated with neurological insult or another medical condition and is not better explained by another mental disorder', value: 23},
+
+    // Social (Pragmatic) Communication Disorder
+    // Criteria A (all of the following symptoms
+    {label: 'Deficits in using communication for social purposes', value: 24},
+    {label: 'Impairment of the ability to change communication to match context or the needs of the listener', value: 25},
+    {label: 'Difficulties following rules for conversation and storytelling', value: 26},
+    {label: 'Difficulties understanding what is not explicitly stated ', value: 27},
+
+    // Criteria B
+    {label: 'Deficits result in functional limitations in effective communication, social participation, social relationships, academic achievement, or occupational performance, individually or in combination', value: 28},
+
+    // Criteria C
+    {label: 'The onset of the symptoms is in the early developmental period', value: 29},
+
+    // Criteria D
+    {label: 'The symptoms are not attributable to another medical or neurological condition or to low abilities in the domains of word structure and grammar, and are not better explained by autism spectrum disorder, intellectual developmental disorde, global developmental delay, or another mental disorder', value: 30},
+
+    // Unspecified Communication Disorder
+    // The unspecified communication disorder category is used in situations in which the clinician chooses not to specify the reason that the criteria are not met for communication disorder or for a specific neurodevelopmental disorder, and includes presentations in which there is insufficient information to make a more specific diagnosis.
+
+    // Autism Spectrum Disorder
+    // Criteria A
+    {label: 'Deficits in social-emotional reciprocity | Abnormal social approach | Failure of normal back-and-forth conversation | Reduced sharing of interests, emotions, or affect | Failure to initiate or respond to social interactions.', value: 31},
+    {label: 'Deficits in nonverbal communicative behaviors used for social interaction | Poorly integrated verbal and nonverbal communication | Abnormalities in eye contact and body language | Deficits in understanding and use of gestures; to a total lack of facial expressions and nonverbal communication', value: 32},
+    {label: 'Deficits in developing, maintaining, and understanding relationships | Difficulties adjusting behavior to suit various social contexts | Difficulties in sharing imaginative play or in making friends; to absence of interest in peers.', value: 33},
+
+    // Criteria B
+    {label: 'Stereotyped or repetitive motor movements, use of objects, or speech | Motor stereotypes | Lining up toys | Flipping objects | Echolalia | Idiosyncratic phrases', value: 34},
+    {label: 'Insistence on sameness, inflexible adherence to routines, or ritualized patterns of verbal or nonverbal behavior | Extreme distress at small changes | Difficulty with transition | Rigid thinking pattern | Greeting rituals | Need to take same route or eat same food every day', value: 35},
+    {label: 'Highly restricted, fixated interests that are abnormal in intensity or focus | Strong attachment | Preoccupation with unusual objects | Excessively circumscribed or perseverative interests', value: 36},
+    {label: 'Hyper- or hyporeactivity to sensory input or unusual interest in sensory aspects of the environment | Parent indifference to pain/temperature | Adverse response to specific sounds | Excessive smelling or touching of objects | Visual fascination with lights or movement', value: 37},
+
+    // Criteria C
+    {label: 'Symptoms must be present in the early developmental period', value: 38},
+
+    // Criteria D
+    {label: 'Symptoms cause clinically significant impairment in social, occupational, or other important areas of current functioning', value: 39},
+
+    // Criteria E
+    {label: 'These disturbances are not better explained by intellectual developmental disorder (intellectual disability) or global developmental delay. Intellectual developmental disorder and autism spectrum disorder frequently co-occur; to make comorbid diagnoses of autism spectrum disorder and intellectual developmental disorder, social communication should be below that expected for general developmental level', value: 40},
+
+    // Attention-Deficit/Hyperactivity Disorder
+    // Criteria A (6 or more symptoms)
+    // Criteria 1A
+    {label: 'Often fails to give close attention to details or makes careless mistakes in schoolwork, at work, or during other activities', value: 41},
+    {label: 'Has difficulty sustaining attention in tasks or play activities ', value: 42},
+    {label: 'Often does not seem to listen when spoken to directly', value: 43},
+    {label: 'Often does not follow through on instructions and fails to finish schoolwork, chores, or duties in the workplace | Starts tasks but quickly loses focus and is easily sidetracked', value: 44},
+    {label: 'Often has difficulty organizing tasks and activities | Difficulty managing sequntial tasks | Difficulty keeping materials and belongings | Messy disorganized work | Poor time management | Fail to meet deadlines', value: 45},
+    {label: 'Often avoids, dislikes, or is reluctant to engage in tasks that require sustained mental effort', value: 46},
+    {label: 'Often loses things necessary for tasks or activities', value: 47},
+    {label: 'Is often easily distracted by extraneous stimuli ', value: 48},
+    {label: 'Is often forgetful in daily activities', value: 49},
+    // Criteria 2A
+    // at least 6 months
+    {label: 'Often fidgets with or taps hands or feet or squirms in seat', value: 50},
+    {label: 'Often leaves seat in situations when remaining seated is expected', value: 52},
+    {label: 'Often runs about or climbs in situations where it is inappropriate', value: 53},
+    {label: 'Often unable to play or engage in leisure activities quietly', value: 54},
+    {label: 'Is often “on the go,” acting as if “driven by a motor', value: 55},
+    {label: 'Often talks excessively', value: 56},
+    {label: 'Often blurts out an answer before a question has been completed', value: 57},
+    {label: 'Often interrupts or intrudes on others', value: 58},
+
+    // Criteria B
+    {label: 'Several inattentive or hyperactive-impulsive symptoms were present prior to age 12 years', value: 59},
+
+    // Criteria C
+    {label: 'There is clear evidence that the symptoms interfere with, or reduce the quality of, social, academic, or occupational functioning', value: 60},
+
+    // Criteria D
+    {label: 'The symptoms do not occur exclusively during the course of schizophrenia or another psychotic disorder and are not better explained by another mental disorder', value: 61},
+
+    // Other Specified Attention Deficit/Hyperactivity Disorder
+    // used in situations in which the clinician chooses to communicate the specific reason that the presentation does not meet the criteria for attention-deficit/hyperactivity disorder or any specific neurodevelopmental disorder.
+
+    // "Unspecified Attention-Deficit/Hyperactivity Disorder
+    // "used in situations in which the clinician chooses not to specify the reason that the criteria are not met for attention-deficit/hyperactivity disorder or for a specific neurodevelopmental disorder, and includes presentations in which there is insufficient information to make a more specific diagnosis.
+
+    // Specific Learning Disorder
+    // atleast 6 months
+    {label: 'Inaccurate or slow and effortful word reading', value: 62},
+    {label: 'Difficulty understanding the meaning of what is read', value: 63},
+    {label: 'Difficulties with spelling', value: 64},
+    {label: 'Difficulties with written expression', value: 65},
+    {label: 'Difficulties mastering number sense, number facts, or calculation', value: 66},
+    {label: 'Difficulties with mathematical reasoning', value: 67},
+
+    // Developmental Coordination Disorder
+    // Criteria A
+    {label: 'The acquisition and execution of coordinated motor skills is substantially below that expected given the individual’s chronological age and opportunity for skill learning and use', value: 68},
+
+    // Criteria B
+    {label: 'The motor skills deficit in Criterion A significantly and persistently interferes with activities of daily living appropriate to chronological age and impacts academic/school productivity, prevocational and vocational activities, leisure, and play', value: 69},
+
+    // Criteria C
+    {label: 'Onset of symptoms is in the early developmental period', value: 70},
+
+    // Criteria D
+    {label: 'The motor skills deficits are not better explained by intellectual developmental disorder (intellectual disability) or visual impairment and are not attributable to a neurological condition affecting movement', value: 71},
+
+    // Stereotypic Movement Disorder
+    // Criteria A
+    {label: 'Repetitive, seemingly driven, and apparently purposeless motor behavior', value: 72},
+
+    // Criteria B
+    {label: 'The repetitive motor behavior interferes with social, academic, or other activities and may result in self-injury', value: 73},
+
+    // Criteria C
+    {label: 'Onset is in the early developmental period', value: 74},
+
+    // Criteria D
+    {label: 'Repetitive motor behavior is not attributable to the physiological effects of a substance or neurological condition and is not better explained by another neurodevelopmental or mental disorder', value: 75},
+
+    // Tourette’s Disorder
+    // Criteria A
+    {label: 'Both multiple motor and one or more vocal tics have been present at some time during the illness, although not necessarily concurrently', value: 76},
+
+    // Criteria B
+    {label: 'The tics may wax and wane in frequency but have persisted for more than 1 year since first tic onset', value: 77},
+
+    // Criteria C
+    {label: 'Onset is before age 18 years', value: 78},
+
+    // Criteria D
+    {label: 'The disturbance is not attributable to the physiological effects of a substance', value: 79},
+    {label: 'Anxiety', value: 80},
+    {label: 'Anxiety', value: 81},
+    {label: 'Anxiety', value: 82},
+    {label: 'Anxiety', value: 83},
+    {label: 'Anxiety', value: 84},
+    {label: 'Anxiety', value: 84},
+
+
+    // Other Specified Tic Disorder
+    // The other specified tic disorder category is used in situations in which the clinician chooses to communicate the specific reason that the presentation does not meet the criteria for a tic disorder or any specific neurodevelopmental disorder. This is done by recording “other specified tic disorder” followed by the specific reason
+
+    // Unspecified Neurodevelopmental Disorder
+    // "The unspecified neurodevelopmental disorder category is used in situations in which the clinician chooses not to specify the reason that the criteria are not met for a specific neurodevelopmental disorder and includes presentations in which there is insufficient information to make a more specific diagnosis (e.g., in emergency room settings).
+]
