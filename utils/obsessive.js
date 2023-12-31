aff74cea19a37753e072bd726e1d2cd0c9f9da31
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2023-12-31 22:41:52 +0800

    Create obsessive.js

---
+++ utils/obsessive.js
@@ -0,0 +1,118 @@
+export const obsessive = [
+    // Obsessive-Compulsive Disorder
+    // Criteria A
+    {label: 'Recurrent and persistent thoughts, urges, or images that are experienced, at some time during the disturbance, as intrusive and unwanted, and that in most individuals cause marked anxiety or distress', value: 1},
+    {label: 'Attempts to ignore or suppress such thoughts, urges, or images, or to neutralize them with some other thought or action | Performing compulsion', value: 2},
+    {label: 'Repetitive behaviors', value: 3},
+    {label: 'The behaviors aimed at preventing or reducing anxiety or distress, or preventing some dreaded event or situation', value: 4},
+
+    // Criteria B
+    {label: 'The obsessions or compulsions are time-consuming and causes clinically significant distress or impairment in social, occupational and other functioning', value: 5},
+
+    // Criteria C
+    {label: 'The obsessive-compulsive symptoms are not attributable to the physiological effects of a substance', value: 6},
+
+    // Criteria D
+    {label: 'The disturbance is not better explained by the symptoms of another mental disorder', value: 7},
+
+    // Body Dysmorphic Disorder
+    // Criteria A
+    {label: 'Preoccupation with one or more perceived defects or flaws in physical appearance that are not observable or appear slight to others', value: 8},
+
+    // Criteria B
+    {label: 'During the course of the disorder, the individual has performed repetitive behaviors', value: 9},
+
+    // Criteria C
+    {label: 'Causes clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 10},
+
+    // Criteria D
+    {label: 'The appearance preoccupation is not better explained by concerns with body fat or weight in an individual whose symptoms meet diagnostic criteria for an eating disorder', value: 11},
+
+    // Hoarding Disorder
+    // Criteria A
+    {label: 'Difficulty discarding or parting with possessions, regardless of their actual value', value: 12},
+
+    // Criteria B
+    {label: 'This difficulty is due to a perceived need to save the items and to distress associated with discarding them', value: 13},
+
+    // Criteria C
+    {label: 'Difficulty discarding possessions results in the accumulation of possessions that congest and clutter active living areas and substantially compromises their intended use', value: 14},
+
+    // Criteria D
+    {label: 'The hoarding causes clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 15},
+
+    // Criteria E
+    {label: 'The hoarding is not attributable to another medical condition', value: 16},
+
+    // Criteria F
+    {label: 'The hoarding is not better explained by the symptoms of another mental disorder', value: 17},
+
+    // Trichotillomania (Hair-Pulling Disorder)
+    // Criteria A
+    {label: 'Recurrent pulling out of one’s hair, resulting in hair loss', value: 18},
+
+    // Criteria B
+    {label: 'The hair pulling causes clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 19},
+
+    // Criteria C
+    {label: 'The hair pulling or hair loss is not attributable to another medical condition', value: 20},
+
+    // Criteria D
+    {label: 'The hair pulling is not better explained by the symptoms of another mental disorder', value: 21},
+
+    // Excoriation (Skin-Picking) Disorder
+    // Criteria A
+    {label: 'Recurrent skin picking resulting in skin lesions', value: 22},
+
+    // Criteria B
+    {label: 'Repeated attempts to decrease or stop skin picking', value: 23},
+
+    // Criteria C
+    {label: 'The skin picking causes clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 24},
+
+    // Criteria D
+    {label: 'The skin picking is not attributable to the physiological effects of a substance', value: 25},
+
+    // Criteria E
+    {label: 'The skin picking is not better explained by symptoms of another mental disorder', value: 26},
+
+    // Substance/Medication-Induce Obsessive-Compulsive and Related Disorder
+    // Criteria A
+    {label: 'Obsessions, compulsions, skin picking, hair pulling, other body-focused repetitive behaviors, or other symptoms characteristic of the obsessive-compulsive and related disorders predominate in the clinical picture', value: 27},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A developed during or soon after substance intoxication or withdrawal or after exposure to or withdrawal from a medication', value: 28},
+    {label: 'The involved substance/medication is capable of producing the symptoms in Criterion A', value: 29},
+
+    // Criteria C
+    {label: 'The disturbance is not better explained by an obsessivecompulsive and related disorder that is not substance/medication-induced', value: 30},
+
+    // Criteria D
+    {label: 'The disturbance does not occur exclusively during the course of a delirium', value: 31},
+
+    // Criteria E
+    {label: 'The disturbance causes clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 32},
+
+    // Obsessive-Compulsive and Related Disorder Due to Another Medical Condition
+    // Criteria A
+    {label: 'Obsessions, compulsions, skin picking, hair pulling, other body-focused repetitive behaviors, or other symptoms characteristic of the obsessive-compulsive and related disorders predominate in the clinical picture', value: 33},
+
+    // Criteria B
+    {label: 'There is evidence from the history, physical examination, or laboratory findings that the disturbance is the direct pathophysiological consequence of another medical condition', value: 34},
+
+    // Criteria C
+    {label: 'The disturbance is not better explained by an obsessive compulsive and related disorder that is not substance/medication-induced', value: 35},
+
+    // Criteria D
+    {label: 'The disturbance does not occur exclusively during the course of a delirium', value: 36},
+
+    // Criteria E
+    {label: 'The disturbance causes clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 37},
+
+    {label: 'Nystagmus', value: 38},
+    {label: 'Nystagmus', value: 39},
+    {label: 'Nystagmus', value: 40},
+
+    // Unspecified Obsessive-Compulsive and Related Disorder
+    // This category applies to presentations in which symptoms characteristic of an obsessive-compulsive and related disorder that cause clinically significant distress or impairment in social, occupational, or other important areas of functioning predominate but do not meet the full criteria for any of the disorders in the obsessive-compulsive and related disorders diagnostic class.
+]
