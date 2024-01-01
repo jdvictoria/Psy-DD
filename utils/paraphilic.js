93bff077c86dcdbc34c0e5a093ca2208c8756cb8
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-01-01 17:08:35 +0800

    Update paraphilic.js

--- utils/paraphilic.js
+++ utils/paraphilic.js
@@ -1,5 +1,80 @@
 export const paraphilicSymptoms = [
-    {label: 'Nystagmus', value: 1},
-    {label: 'Nystagmus', value: 2},
-    {label: 'Nystagmus', value: 3},
+    // Voyeuristic Disorder
+    // Criteria A
+    // 6 months
+    {label: 'Recurrent and intense sexual arousal from observing an unsuspecting person who is naked, in the process of disrobing, or engaging in sexual activity', value: 1},
+
+    // Criteria B
+    {label: 'Has acted on these sexual urges with a nonconsenting person, or the sexual urges or fantasies cause clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 2},
+
+    // Criteria C
+    {label: 'Experiencing arousal and/or acting on the urges is at least 18 years of age', value: 3},
+
+    // Exhibitionistic Disorder
+    // 6 months
+    // Criteria A
+    {label: 'Recurrent and intense sexual arousal from the exposure of one’s genitals to an unsuspecting person, as manifested by fantasies, urges, or behaviors', value: 4},
+
+    // Criteria B
+    {label: 'Acted on these sexual urges with a nonconsenting person, or the sexual urges or fantasies cause clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 5},
+
+    // Frotteuristic Disorder
+    // 6 months
+    // Criteria A
+    {label: 'Recurrent and intense sexual arousal from touching or rubbing against a nonconsenting person', value: 6},
+
+    // Criteria B
+    {label: 'Acted on these sexual urges with a nonconsenting person, or the sexual urges or fantasies cause clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 7},
+
+    // Sexual Masochism Disorder
+    // Criteria A
+    // 6 months / severe
+    {label: 'Recurrent and intense sexual arousal from the act of being humiliated, beaten, bound, or otherwise made to suffer, as manifested by fantasies, urges, or behaviors', value: 8},
+
+    // Criteria B
+    {label: 'The fantasies, sexual urges, or behaviors cause clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 9},
+
+    // Sexual Sadism Disorder
+    // Criteria A
+    // 6 months
+    {label: 'Recurrent and intense sexual arousal from the physical or psychological suffering of another person, as manifested by fantasies, urges, or behaviors', value: 10},
+
+    // Criteria B
+    {label: 'The individual has acted on these sexual urges with a nonconsenting person, or the sexual urges or fantasies cause clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 11},
+
+    // Pedophilic Disorder
+    // Criteria A
+    // 6 months
+    {label: 'Recurrent, intense sexually arousing fantasies, sexual urges, or behaviors involving sexual activity with a prepubescent child or children', value: 12},
+
+    // Criteria B
+    {label: 'Individual has acted on these sexual urges, or the sexual urges or fantasies cause marked distress or interpersonal difficulty', value: 13},
+
+    // Criteria C
+    {label: 'The individual is at least age 16 years and at least 5 years older than the child or children in Criterion A', value: 14},
+
+    // Fetishistic Disorder
+    // Criteria A
+    // 6 months
+    {label: 'Recurrent and intense sexual arousal from either the use of nonliving objects or a highly specific focus on nongenital body part(s), as manifested by fantasies, urges, or behaviors', value: 15},
+
+    // Criteria B
+    {label: 'The fantasies, sexual urges, or behaviors cause clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 16},
+
+    // Criteria C
+    {label: 'The fetish objects are not limited to articles of clothing used in cross-dressing', value: 17},
+
+    // Transvestic Disorder
+    // Criteria A
+    // 6 months
+    {label: 'Recurrent and intense sexual arousal from cross-dressing, as manifested by fantasies, urges, or behaviors', value: 18},
+
+    // Criteria B
+    {label: 'The fantasies, sexual urges, or behaviors cause clinically significant distress or impairment in social, occupational, or other important areas of functioning', value: 19},
+
+    // Other Specified Paraphilic Disorder
+    // This category applies to presentations in which symptoms characteristic of a paraphilic disorder that cause clinically significant distress or impairment in social, occupational, or other important areas of functioning predominate but do not meet the full criteria for any of the disorders in the paraphilic disorders diagnostic class.
+
+    // Unspecified Paraphilic Disorder
+    // This category applies to presentations in which symptoms characteristic of a paraphilic disorder that cause clinically significant distress or impairment in social, occupational, or other important areas of functioning predominate but do not meet the full criteria for any of the disorders in the paraphilic disorders diagnostic class.
 ]
