258f8cf1e7dcde6bffe5eb6df4134543508b569e
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-01-02 04:08:32 +0800

    Add Symptoms Card

--- components/atoms/card-diagnose_big/index.tsx
+++ components/atoms/card-diagnose_big/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {DiagnoseSymptomCard, StyledRow, StyledTouchableRow} from '../../../styles/form-container';
 import {contentText, StyledText16} from "../../../styles/form-text";
@@ -17,49 +17,67 @@ const dummyData = [
 // @ts-ignore
 function CardDiagnoseBig({isDarkMode, filter, bigCardCount}) {
     const contentStyle = contentText(isDarkMode);
+    const [additionalCardCount, setAdditionalCardCount] = useState(1);
 
-  let symptomData;
-  let durationData;
-  let severityData;
-  let drugData;
-  let distressData;
+    let symptomData;
+    let durationData;
+    let severityData;
+    let drugData;
+    let distressData;
 
-  if (filter === 'a') {
-      symptomData = traumaSymptoms;
-      durationData = dummyData;
-      severityData = dummyData;
-      drugData = dummyData;
-      distressData = dummyData;
-  } else {
-      symptomData = dummyData;
-      durationData = dummyData;
-      severityData = dummyData;
-      drugData = dummyData;
-      distressData = dummyData;
-  }
+    if (filter === 'a') {
+        symptomData = traumaSymptoms;
+        durationData = dummyData;
+        severityData = dummyData;
+        drugData = dummyData;
+        distressData = dummyData;
+    } else {
+        symptomData = dummyData;
+        durationData = dummyData;
+        severityData = dummyData;
+        drugData = dummyData;
+        distressData = dummyData;
+    }
+
+    const handleAddSymptomPress = () => {
+        setAdditionalCardCount((prevCount) => prevCount + 1);
+    };
+
+    const renderCards = () => {
+        const additionalCards = [];
+        for (let i = 0; i < additionalCardCount; i++) {
+            additionalCards.push(
+                <DiagnoseSymptomCard
+                    key={`additionalCard-${i}`}
+                    style={{
+                        backgroundColor: isDarkMode ? '#1A2230' : '#FFFFFF',
+                        marginTop: 10, // Adjust the spacing as needed
+                    }}
+                >
+                    <StyledRow>
+                        <DropdownComponent isDarkMode={isDarkMode} data={symptomData} label={'Symptom/s'} value={null} setValue={null} />
+                    </StyledRow>
+                    <StyledRow>
+                        <DropdownComponent isDarkMode={isDarkMode} data={durationData} label={'Duration'} value={null} setValue={null} />
+                        <DropdownComponent isDarkMode={isDarkMode} data={severityData} label={'Severity'} value={null} setValue={null} />
+                    </StyledRow>
+                    <StyledRow>
+                        <DropdownComponent isDarkMode={isDarkMode} data={drugData} label={'Drug Influenced?'} value={null} setValue={null} />
+                        <DropdownComponent isDarkMode={isDarkMode} data={distressData} label={'Causes Distress?'} value={null} setValue={null} />
+                    </StyledRow>
+                </DiagnoseSymptomCard>
+            );
+        }
+        return additionalCards;
+    };
 
   return (
     <>
         <StyledRow style={{justifyContent: 'flex-start', width: '90%'}}>
             <StyledText16 style={[contentStyle.bold]}>Criteria {bigCardCount.toUpperCase()}</StyledText16>
         </StyledRow>
-        <DiagnoseSymptomCard
-            style={{
-                backgroundColor: isDarkMode ? '#1A2230' : '#FFFFFF',
-            }}>
-            <StyledRow>
-                <DropdownComponent isDarkMode={isDarkMode} data={symptomData} label={'Symptom/s'} value={null} setValue={null} />
-            </StyledRow>
-            <StyledRow>
-                <DropdownComponent isDarkMode={isDarkMode} data={durationData} label={'Duration'} value={null} setValue={null} />
-                <DropdownComponent isDarkMode={isDarkMode} data={severityData} label={'Severity'} value={null} setValue={null} />
-            </StyledRow>
-            <StyledRow>
-                <DropdownComponent isDarkMode={isDarkMode} data={drugData} label={'Drug Influenced?'} value={null} setValue={null} />
-                <DropdownComponent isDarkMode={isDarkMode} data={distressData} label={'Causes Distress?'} value={null} setValue={null} />
-            </StyledRow>
-        </DiagnoseSymptomCard>
-        <StyledTouchableRow style={{width: '90%', marginTop: 5, marginBottom: 10}}>
+        {renderCards()}
+        <StyledTouchableRow style={{width: '90%', marginTop: 5, marginBottom: 10}} onPress={handleAddSymptomPress}>
             <StyledText16 style={[contentStyle.bold, {color: "#959595"}]}>Add Symptom</StyledText16>
         </StyledTouchableRow>
     </>
