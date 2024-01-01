f4c4fe29ee04bc8bb7c1992ce76b141a8820d3e9
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-01-01 22:41:06 +0800

    Create sexual.js

---
+++ utils/sexual.js
@@ -0,0 +1,124 @@
+export const sexualSymptoms = [
+    // Delayed Ejaculation
+    // Criteria A (One Symptom)
+    // 6 months
+    {label: 'Marked delay in ejaculation', value: 1},
+    {label: 'Marked infrequency or absence of ejaculation', value: 2},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A have persisted for a minimum duration of approximately 6 months', value: 3},
+
+    // Criteria C
+    {label: 'The symptoms in Criterion A cause clinically significant distress in the individual', value: 4},
+
+    // Criteria D
+    {label: 'The sexual dysfunction is not better explained by a nonsexual mental disorder or as a consequence of severe relationship distress or other significant stressors and is not attributable to the effects of a substance/medication or another medical condition', value: 5},
+
+    // Erectile Disorder
+    // Criteria A (One of three symptoms must bw 75 or 100% experience
+    // 6 months
+    {label: 'Difficulty in obtaining an erection during sexual activity', value: 6},
+    {label: 'Difficulty in maintaining an erection until the completion of sexual activity', value: 7},
+    {label: 'Decrease in erectile rigidity', value: 8},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A have persisted for a minimum duration of approximately 6 months', value: 9},
+
+    // Criteria C
+    {label: 'The symptoms in Criterion A cause clinically significant distress in the individual', value: 10},
+
+    // Criteria D
+    {label: 'The sexual dysfunction is not better explained by a nonsexual mental disorder or as a consequence of severe relationship distress or other significant stressors and is not attributable to the effects of a substance/medication or another medical condition', value: 11},
+
+    // Female Orgasmic Disorder
+    // Criteria A (One of three symptoms must bw 75 or 100% experience
+    // 6 months
+    {label: 'Marked delay in, marked infrequency of, or absence of orgasm', value: 12},
+    {label: 'Markedly reduced intensity of orgasmic sensations', value: 13},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A have persisted for a minimum duration of approximately 6 months', value: 14},
+
+    // Criteria C
+    {label: 'The symptoms in Criterion A cause clinically significant distress in the individual', value: 15},
+
+    // Criteria D
+    {label: 'The sexual dysfunction is not better explained by a nonsexual mental disorder or as a consequence of severe relationship distress or other stresors and not attrubutable to subtance or medical condition', value: 16},
+
+    // Female Sexual Interest/Arousal Disorder
+    // Criteria A ( three symptoms
+    {label: 'Absent/reduced interest in sexual activity', value: 17},
+    {label: 'Absent/reduced sexual/erotic thoughts or fantasies', value: 18},
+    {label: 'Reduced initiation of sexual activity | Typically unreceptive to a partner’s attempts to initiate', value: 19},
+    {label: 'Reduced sexual excitement/pleasure during sexual activity in almost all or all (approximately 75%–100%) sexual encounters', value: 20},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A have persisted for a minimum duration of approximately 6 months', value: 21},
+
+    // Criteria C
+    {label: 'The symptoms in Criterion A cause clinically significant distress in the individual', value: 22},
+
+    // Criteria D
+    {label: 'The sexual dysfunction is not better explained by a nonsexual mental disorder or as a consequence of severe relationship distress or other stresors and not attrubutable to subtance or medical condition', value: 23},
+
+    // Genito-Pelvic Pain/Penetration Disorder
+    // Criteria A
+    // 6 months
+    {label: 'Vaginal penetration during intercourse', value: 24},
+    {label: 'Pelvic pain during vaginal intercourse or penetration attempts', value: 25},
+    {label: 'Anxiety about vulvovaginal or pelvic pain in anticipation of vaginal penetration', value: 26},
+    {label: 'Tensed or tightening of pelvic muscle during attemp of vaginal intercourse', value: 27},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A have persisted for a minimum duration of approximately 6 months', value: 28},
+
+    // Criteria C
+    {label: 'The symptoms in Criterion A cause clinically significant distress in the individual', value: 29},
+
+    // Criteria D
+    {label: 'The sexual dysfunction is not better explained by a nonsexual mental disorder or as a consequence of severe relationship distress or other stresors and not attrubutable to subtance or medical condition', value: 30},
+
+    // Male Hypoactive Sexual Desire Disorder
+    // Criteria A
+    {label: 'Persistently or recurrently deficient (or absent) sexual/erotic thoughts or fantasies and desire for sexual activity', value: 31},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A have persisted for a minimum duration of approximately 6 months', value: 32},
+
+    // Criteria C
+    {label: 'The symptoms in Criterion A cause clinically significant distress in the individual', value: 33},
+
+    // Criteria D
+    {label: 'The sexual dysfunction is not better explained by a nonsexual mental disorder or as a consequence of severe relationship distress or other significant stressors and is not attributable to the effects of a substance/medication or another medical condition', value: 34},
+
+    // Premature (Early) Ejaculation
+    // Criteria A
+    // 6 months
+    {label: 'Recurrent pattern of ejaculation during sexual activitywithin approximately 1 minute following vaginal penetration and before the individual wishes it', value: 35},
+
+    // Criteria B
+    {label: 'The symptom in Criterion A must have been present for at least 6 months and must be experienced on almost all or all (approximately 75%–100%) occasions of sexual activity', value: 36},
+
+    // Criteria C
+    {label: 'The symptom in Criterion A causes clinically significant distress in the individual', value: 37},
+
+    // Criteria D
+    {label: 'The sexual dysfunction is not better explained by a nonsexual mental disorder,  consequence of severe relationship distress or other significant stressors and is not attributable to the effects of a substance/medication or another medical condition', value: 38},
+
+    // Substance/Medication-Induced Sexual Dysfunction
+    // Criteria A
+    {label: 'A clinically significant disturbance in sexual function', value: 39},
+
+    // Criteria B
+    {label: 'The symptoms in Criterion A developed during or soon after substance intoxication or withdrawal or after exposure to or withdrawal from a medication', value: 40},
+    {label: 'The involved substance/medication is capable of producing the symptoms in Criterion A', value: 41},
+
+    // Criteria C
+    {label: 'The disturbance is not better explained by a sexual dysfunction that is not substance/medication-induced', value: 42},
+
+    // Criteria D
+    {label: 'The disturbance does not occur exclusively during the course of a delirium', value: 43},
+
+    // Criteria E
+    {label: 'The disturbance causes clinically significant distress in the individual', value: 44},
+]
