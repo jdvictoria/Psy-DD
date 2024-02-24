d735b8dbf0e5863d628ac8ec7ade053a5fa8897f
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-02-24 22:19:06 +0800

    Final Disruptive

--- components/molecules/diagnose/index.tsx
+++ components/molecules/diagnose/index.tsx
@@ -102,6 +102,7 @@ function HomeDiagnose({isDarkMode}) {
           setResult(anxietyDiagnosis(numbers));
           break;
         case 'l':
+          setResult(disruptiveDiagnosis(numbers));
           break;
         case 'm':
           setResult(disruptiveDiagnosis(numbers));
